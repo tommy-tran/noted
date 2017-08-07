06034e88878832fb8b07c29e729d8d6214ca7efe
tommy-tran <tome7@live.com>
2017-08-07 19:28:05 -0400

    Font and heading size change

--- app/index.php
+++ app/index.php
@@ -4,9 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Raleway|Roboto" rel="stylesheet">
     <link rel="stylesheet" href="temp/styles/styles.css">
     <title>Notepad</title>
     <script src="/temp/scripts/Vendor.js"></script>
+    
   </head>
   <body>
 
