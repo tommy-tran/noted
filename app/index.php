3284abb525953bfd9ce3322cacda37fb1d752a68
tommy-tran <tome7@live.com>
2017-08-07 19:18:02 -0400

    Header started, basic layout and menu icon created

--- app/index.php
+++ app/index.php
@@ -9,11 +9,25 @@
     <script src="/temp/scripts/Vendor.js"></script>
   </head>
   <body>
-    <nav>
-      <div class="header">
-        Notepad
+
+  <header class="header">
+    <div class="header-container">
+      <span>Header</span>
+      <nav class="primary-nav">
+        <ul>
+        <!--Links-->
+          <li><a href="#">LINK1</a></li>
+          <li><a href="#">LINK2</a></li>
+          <li><a href="#">LINK3</a></li>
+        <!--Icon-->
+        </ul>
+      </nav>
+
+      <div class="header__menu-icon">
+        <div class="header__menu-icon__middle"></div>
       </div>
-    </nav>
+    </div>
+  </header>
     <h1>Hello, world!</h1>
 
     <script src="/temp/scripts/App.js"></script>
