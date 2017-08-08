d7dec51356ce011a3aca16f90e99fa9e1903914a
tommy-tran <tome7@live.com>
2017-08-07 20:24:58 -0400

    Installed mixins

--- gulp/styles.js
+++ gulp/styles.js
@@ -4,10 +4,11 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, autoprefixer, nested]))
+        .pipe(postcss([cssImport, mixins, cssvars, autoprefixer, nested]))
         .on('error', function(error) {
             console.log(error.toString());
             this.emit('end');
