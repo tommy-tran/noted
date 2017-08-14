8b25056b2c8d8f1e6a22362e1bacc7b15cd36275
tommy-tran <tome7@live.com>
2017-08-14 01:23:24 -0400

    Changed modal settings at different widths

--- app/assets/scripts/modules/Requests.js
+++ app/assets/scripts/modules/Requests.js
@@ -3,7 +3,6 @@ import $ from 'jquery';
 $("#signupform").submit(function(e) {
     e.preventDefault();
     var datatopost = $(this).serializeArray();
-    console.log(datatopost);
     // send to signup.php
     $.ajax({
         url: "signup.php",
@@ -11,13 +10,11 @@ $("#signupform").submit(function(e) {
         data: datatopost,
         success: function(data) {
             if (data) {
-                console.log("Success: " + data);
                 $(".message").removeClass("message--hidden");
                 $(".message-signup").html(data);
             }
         },
         error: function(data) {
-            console.log("Error: " + data);
             $(".message").removeClass("message--hidden");
             $(".message-signup").html("<div class='message-signup'>There was an error with the Ajax Call. Please try again later.</div>");
         }
