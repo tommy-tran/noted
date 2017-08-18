692e6fcbd56167388952d3ee78e89a3a5aec6663
tommy-tran <tome7@live.com>
2017-08-18 03:43:17 -0400

    Some additional styling to notes and deleting empty notes

--- app/loadnotes.php
+++ app/loadnotes.php
@@ -11,12 +11,15 @@ echo "
     <div class='notes__item-date'>August 14, 2017 02:00:00</div>
 </div>
 <div class='notes__item'>
-    <div class='notes__item-title'>This is the first note</div>
+    <div class='notes__item-title'>This is the first note. The length of the title is pretty long so I don't really understand what I should do here.</div>
     <div class='notes__item-date'>August 12, 2017 02:00:00</div>
 </div>
 ";
 // Get user_id
+$user_id = $SESSION['user_id'];
 // Delete empty notes
+$sql = "DELETE FROM notes WHERE note=''";
+$result = mysqli_query($link, $sql);
 // Get notes
 // Show notes
 ?>
