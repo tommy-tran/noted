d4873132498648b5d73a23cbfe068e31ce0ca998
tommy-tran <tome7@live.com>
2017-08-19 03:36:20 -0400

    Handling tab in the notepad

--- app/assets/scripts/modules/Notes.js
+++ app/assets/scripts/modules/Notes.js
@@ -10,21 +10,27 @@ $(document).ready(function(){
         }
     });
 
-    // setInterval(function() {
-    //     $(".notes__item").click(function() {
-    //         var main = $('.notes__extra-main');
-    //         main.addClass("notes--hidden");
-    //         var edit = $(".notes__extra-edit");
-    //         edit.addClass("notes--hidden");
-    //         var add = $(".notes__extra-add");
-    //         add.removeClass("notes--hidden");
-    //         var noteList = $(".notes__list");
-    //         noteList.addClass("notes__list--hide");
-    //         var activeNote = $(this).attr("id");
-    //         $("#notepad").val($(this).find('.notes__item-note').text());
-    //         $(".notes__notepad").removeClass("notes__notepad--hide");
-    //     });    
-    // }, 1000);
+    $("#notepad").keydown(function(e) {
+    if(e.keyCode === 9) { // tab was pressed
+        // get caret position/selection
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+
+        var $this = $(this);
+        var value = $this.val();
+
+        // set textarea value to: text before caret + tab + text after caret
+        $this.val(value.substring(0, start)
+                    + "\t"
+                    + value.substring(end));
+
+        // put caret at right position again (add one for the tab)
+        this.selectionStart = this.selectionEnd = start + 1;
+
+        // prevent the focus lose
+        e.preventDefault();
+    }
+});
     
 });
 
