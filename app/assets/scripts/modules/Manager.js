ccb9bf70a4adbed1c5c729aa822c06f9611adb2d
tommy-tran <tome7@live.com>
2017-08-22 01:59:53 -0400

    Fixed new loaded notes not being deleted

--- app/assets/scripts/modules/Manager.js
+++ app/assets/scripts/modules/Manager.js
@@ -37,18 +37,48 @@ class Manager {
         this.noteList.removeClass("notes__list--hide");
         this.notepadtext.val("");
         this.noteTitle.text("Title");
+        var that = this;
         $.ajax({
             url: "loadnotes.php",
             success: function(data) {
                 // Clear notepad
                 console.log("Success: Loaded Notes!");
                 $(".notes__list").html(data);
+                that.deleteNotes();
             },
             error: function(data) {
                 console.log(data);
             }
         });
     }
+
+    deleteNotes() {
+        $(".notes__item-close").click(function(e) {
+            e.stopPropagation();
+            // Get note
+            var note = $(this).parent();
+            // Get note item's id
+            var id = note.attr('id');
+            // Call to deletenotes
+            $.ajax({
+                url: "deletenote.php",
+                type: "POST",
+                data: {id:id},
+                success: function(data) {
+                    if (data =='error') {
+                        console.log('Error deleting note: ' + id);
+                    } else {
+                        console.log("Success: Deleted Note: " + id);
+                        note.remove();
+                    }
+                },
+                error: function(data) {
+                    console.log(data);
+                }
+            
+            });
+        }
+    )};
 
     openAdd() {
         this.main.addClass("notes--hidden");
